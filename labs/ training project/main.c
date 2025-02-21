15247f1117b940f9205c0f2c331b952be06e73cf
POPO6432 <cozta146@gmail.com>
2025-02-21 17:33:11 +1000

    Update main.c

--- labs/ training project/main.c
+++ labs/ training project/main.c
@@ -2,27 +2,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 int main() {
-	bool isHasCar= true;
+	const short a=0;
+	
+	double x, y,res;
+	printf("Vvedite x:\n");
+	scanf("%lf",&x);
+	printf("Vvedite y:\n");
+	scanf("%lf",&y);
+	
+	res=x/y;
+	printf("Result:%lf\n",res);
+	
+	
+	//char s='L';
+	//printf("%c",s);
+	
+	
+	
+	
+	
+	//bool isHasCar= true;
 	
 	// -32 768 32 768
-	short s=500;
+	//short s=500;
 	// 0-64 000
-	unsigned short a=5;
+	//unsigned short a=5;
 	// -2mln 2mln
-	int x=5,y=6,res;
+	//int x=5,y=6,res;
 	
-	float a1=4.5f;
-	double a2=4.5345f;
+	//float a1=4.5f;
+	//double a2=4.5345f;
 	
-	char sym='K';
+	//char sym='K';
 	
 	
 
-	x*=5;
-	res=x+y;
+	//x*=5;
+	//res=x+y;
 
-	printf("Hello World\n");
-	printf("peremennaya:%d+%d=%d\n",x,y,res);
+	//printf("Hello World\n");
+	//printf("peremennaya:%d+%d=%d\n",x,y,res);
 	
 	
 	
