924d21508a9fa620663cb5d10f66d30c99c64c8c
POPO6432 <cozta146@gmail.com>
2025-02-21 16:52:43 +1000

    Update main.c

--- labs/ training project/main.c
+++ labs/ training project/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+	// -32 768 32 768
+	short s=500;
+	// 0-64 000
+	unsigned short a=5;
+	// -2mln 2mln
+	int x=5,y=6,res;
+	
+	float a1=4.5f;
+	double a2=4.5345f;
+	
+
+	x*=5;
+	res=x+y;
+
+	printf("Hello World\n");
+	printf("peremennaya:%d+%d=%d\n",x,y,res);
+	
+	
+	
+	return 0;
+}
