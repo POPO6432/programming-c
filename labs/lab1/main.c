15f5c3e412ad599872e52f3e29218810378fabc3
POPO6432 <cozta146@gmail.com>
2025-02-21 20:16:51 +1000

    Update main.c

--- labs/lab1/main.c
+++ labs/lab1/main.c
@@ -1,43 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+#include <stdbool.h>
+/* ЗАДАНИЕ 1
+	Объявить вещественные переменные a, b и с и задать их значения. Предполагая, что a, b, c есть коэффициенты квадратного уравнения вывести на консоль значения их корней х1, х2.
+	Следует подобрать такие значения коэффициентов, при которых корни будут существовать.
+	Примечание. Для выполнения задания потребуется функции вычисления квад-ратного корня (возведение в степень), а так же вывод данных на консоль.
+	Возведение в степень - подключаем заголовочный файл	 math.h и исполь-зуем функцию pow, первым параметром которого должен быть возводимое значение, а вторым - степень, тип данных double.
+	Вывод данных - заголовочный файл stdio.h, функция printf, первым параметром является форматная строка, а последующие - переменные, значения которых необхо-димо вывести.
+	Допилить прогу зациклить её с сообщением продолжить да\нет?
+*/
 int main(int argc, char *argv[]) {
-	double a,b,c,x1,x2,d;
-	printf("Vvedite a:\n");
-	scanf("%lf",&a);
-	
-	printf("Vvedite b:\n");
-	scanf("%lf",&b);
-	
-	printf("Vvedite c:\n");
-	scanf("%lf",&c);
+    double a, b, c, x1, x2, d;
+    char ch;
+    bool p=true;
+    while (p) {
+        printf("Vvedite a:\n");
+        scanf("%lf", &a);
 
-	d=pow(b,2)-4*a*c;
-	
-	if(0>d){
-		printf("KORNEY NET");
-	}
-	
-	
-	if (d==0){
-		x1=-d/(2*a);
-		printf("x1=%lf\n",x1);
-		
-	}
-	else
-	{
-	
-	
-	x1=-b+sqrt(d)/(2*a);
-	x2=-b-sqrt(d)/(2*a);
-	
-	printf("x1=%lf\n",x1);
-	printf("x2=%lf\n",x2);
-	printf("d=%lf\n",d);
-	}
-	
-	
-	return 0;
+        printf("Vvedite b:\n");
+        scanf("%lf", &b);
+
+        printf("Vvedite c:\n");
+        scanf("%lf", &c);
+
+        d = pow(b, 2) - 4 * a * c;
+
+        if (d < 0) {
+            printf("KORNEY NET\n");
+        } else if (d == 0) {
+            x1 = -b / (2 * a);
+            printf("x1 = %lf\n", x1);
+        } else {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            x2 = (-b - sqrt(d)) / (2 * a);
+            printf("x1 = %lf\n", x1);
+            printf("x2 = %lf\n", x2);
+        }
+
+        printf("Prodoljaem? (d/n): ");
+        scanf(" %c", &ch);
+
+        if (ch =='n') {
+            break;
+        }
+    }
+
+    return 0;
 }
