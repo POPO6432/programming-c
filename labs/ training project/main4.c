b76cc9f39954b28800890a7141591169ed347a3f
POPO6432 <cozta146@gmail.com>
2025-02-21 19:24:12 +1000

    Add files via upload

---
+++ labs/ training project/main4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main() {
+	//float i;
+	//for(i = 100; i>10;i/=2)
+	//	printf("%.2lf\n",i);
+	//bool isHasCas=true;
+	//int x=0;
+	//while (isHasCas){
+	//	printf("vvedite ");
+	//	scanf("%d\n",&x);
+	//	if(x==1)
+	//	isHasCas=false;		
+	//}
+	float i=1000;
+	while(i>10){
+		printf("%.2f\n",i);
+		i/=20;
+	}		
+	
+	
+	return 0;
+}
